81fcef5cae5df78a4a32c17092202748f408b89e
monsterkos <monsterkos10@gmail.com>
2022-06-10 00:17:31 +0900

    Refactoring domain model and test logic

--- model.py
+++ model.py
@@ -1,35 +1,53 @@
 import uuid
-from pydantic import BaseModel
-from datetime import datetime
-
-from exception import LackOfQuantity
+from dataclasses import dataclass
+from datetime import date
 
 
-class OrderLine(BaseModel):
-    order_id: str = str(uuid.uuid4())
+# 값 객체 (value object) : 내부 데이터에 따라 식별되는 도메인 객체
+@dataclass(
+    frozen=True
+)  # frozen=True 로 해당 객체를 불변 객체로 만들어주며, dict 키로 사용할 수 있고 set 에 add 할 수 있다.
+class OrderLine:
     sku: str
-    quantity: int
+    qty: int
+    order_id: str = str(uuid.uuid4())
 
 
+# 엔티티 (entity) : 일부 값이 바뀌어도 특정 식별 값에 의해 동일하다고 판단할 수 있는 영속적인 정체성을 갖는 도메인 객체
 class Batch:
-    def __init__(self, sku: str, quantity: int, eta: datetime | None = None):
+    def __init__(self, sku: str, qty: int, eta: date | None = None):
         self.ref: str = str(uuid.uuid4())
         self.sku = sku
-        self.available_quantity = quantity
+        self.purchased_quantity = qty
         self.eta = eta
-        self._allocated_orders: set[OrderLine.order_id] = set()
+        self._allocated_orders: set[OrderLine] = set()
 
     def __repr__(self):
         return f"Batch {self.ref}"
 
+    def __eq__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        return self.ref == other.ref
+
+    def __hash__(self):
+        return hash(self.ref)
+
     def allocate(self, order_line: OrderLine) -> None:
-        if self.available_quantity < order_line.quantity:
-            raise LackOfQuantity(
-                f"Available Quantity of {self.sku} is lower than order quantity"
-            )
+        if self.can_allocate(order_line):
+            self._allocated_orders.add(order_line)
 
+    def can_allocate(self, order_line: OrderLine) -> bool:
+        return self.sku == order_line.sku and self.available_quantity >= order_line.qty
+
+    def deallocate(self, order_line: OrderLine) -> None:
         if order_line.order_id in self._allocated_orders:
-            return
+            self._allocated_orders.remove(order_line)
 
-        self.available_quantity -= order_line.quantity
-        self._allocated_orders.add(order_line.order_id)
+    @property
+    def available_quantity(self) -> int:
+        return self.purchased_quantity - self.allocated_quantity
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocated_orders)
--- test/test_domain_model.py
+++ test/test_domain_model.py
@@ -1,7 +1,6 @@
-import pytest
+from datetime import datetime
 
 from model import OrderLine, Batch
-from exception import LackOfQuantity
 
 """
 product(제품) 는 SKU 단위로 식별된다.
@@ -28,38 +27,64 @@ ex)
 
 batch 가 현재 배송 중이면 ETA(Estimated Time of Arrival) 정보가 batch 에 들어있다. ETA 가 없는 batch 는 창고 재고다. 창고 재고를
 배송 중인 batch 보다 먼저 할당해야 한다. 배송 중인 batch 를 할당할 때는 ETA 가 가장 빠른 batch 를 먼저 할당한다.
+
+할당된 order line 을 해제할 수 있으며, 할당되지 않은 order_line 을 해제하면 batch의 가용 수량에 아무 영향이 없어야 한다.
 """
 
 
-def test_allocating_to_a_batch_reduces_the_available_quantity():
-    batch = Batch("SMALL-TABLE", 20)
-    order_line = OrderLine(sku="SMALL-TABLE", quantity=2)
-
-    batch.allocate(order_line)
-
-    assert batch.available_quantity == 18
+def make_temp_batch_and_order_line(
+    sku: str, batch_qty: int, order_qty: int
+) -> tuple[Batch, OrderLine]:
+    return Batch(sku, batch_qty, eta=datetime.now()), OrderLine(sku=sku, qty=order_qty)
 
 
-def test_available_quantity_must_bigger_than_order_line_quantity():
-    batch = Batch("BLUE-CUSHION", 1)
-    order_line = OrderLine(sku="BLUE-CUSHION", quantity=2)
+class TestAllocation:
+    @staticmethod
+    def test_allocating_to_a_batch_reduces_the_available_quantity():
+        batch, order_line = make_temp_batch_and_order_line("SMALL-TABLE", 20, 2)
 
-    with pytest.raises(LackOfQuantity, match="BLUE-CUSHION"):
         batch.allocate(order_line)
 
-    assert batch.available_quantity == 1
+        assert batch.available_quantity == 18
 
+    @staticmethod
+    def test_cannot_allocate_if_available_smaller_than_required():
+        batch, order_line = make_temp_batch_and_order_line("BLUE-CUSHION", 1, 2)
 
-def test_do_not_allocate_same_order_line_twice():
-    batch = Batch("RED-VASE", 10)
-    order_line = OrderLine(sku="RED-VASE", quantity=2)
+        assert batch.can_allocate(order_line) is False
+        assert batch.available_quantity == 1
 
-    batch.allocate(order_line)
-    batch.allocate(order_line)
+    @staticmethod
+    def test_do_not_allocate_same_order_line_twice():
+        batch, order_line = make_temp_batch_and_order_line("RED-VASE", 10, 2)
 
-    assert batch.available_quantity == 8
+        batch.allocate(order_line)
+        batch.allocate(order_line)
 
+        assert batch.available_quantity == 8
 
-# def test_allocating_to_a_batch_warehouse_stock_faster_than_shipping_stock():
-#     warehouse_batch = Batch("batch-004", "TASTLESS-LAMP", 5)
-#     shipping_batch = Batch("batch-005", "TASTLESS-LAMP", 10, datetime(2022, 6, 8))
+    @staticmethod
+    def test_can_allocate_if_available_greater_than_required():
+        batch, order_line = make_temp_batch_and_order_line("ELEGANT-LAMP", 3, 1)
+
+        assert batch.can_allocate(order_line)
+
+    @staticmethod
+    def test_can_allocate_if_available_equal_to_required():
+        batch, order_line = make_temp_batch_and_order_line("ELEGANT-LAMP", 3, 3)
+
+        assert batch.can_allocate(order_line)
+
+    @staticmethod
+    def test_cannot_allocate_if_sku_do_not_match():
+        batch = Batch("BIG-DESK", 10)
+        order_line = OrderLine(sku="SMALL-DESK", qty=2)
+
+        assert batch.can_allocate(order_line) is False
+
+    @staticmethod
+    def test_can_only_deallocate_allocated_line():
+        batch, order_line = make_temp_batch_and_order_line("ELEGANT-LAMP", 3, 3)
+        batch.deallocate(order_line)
+
+        assert batch.available_quantity == 3
